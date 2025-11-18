a9aeb6a617f4de5abbcfc267530106056c6767ee
Judy Sitero <siterojudith@gmail.com>
2025-11-18 11:02:00 -0800

    Add files via upload

--- 02_Product_List.sql
+++ 02_Product_List.sql
@@ -26,10 +26,42 @@ FROM products
 WHERE UnitsInStock >= 100
 ORDER BY UnitPrice DESC, ProductName ASC; 
 
--- Q7 What are the products that we carry where we have no units on hand, but 1 or more units of them on backorder? Order them by product name.
+-- Q7. What are the products that we carry where we have no units on hand, but 1 or more units of them on backorder? Order them by product name.
 SELECT ProductName, UnitsInStock, UnitsOnOrder
 FROM products
 WHERE UnitsInStock = 0 AND UnitsOnOrder > 0 
 ORDER BY ProductName;
 
--- Q8 What is the name of the table that holds the types (categories) of the items Northwind sells?
+-- Q8. What is the name of the table that holds the types (categories) of the items Northwind sells?
+-- It's called Categories
+
+-- Q9. Write a query that lists all of the columns and all the rows of the categories table? What is the category id of seafood?
+SELECT * 
+FROM categories;
+-- The category id for seafood is 8 
+
+-- Q10. Examine the Products table. How does it identify the type (category) of each item sold? Write a query to list all of the seafood items we carry.
+SELECT ProductName, UnitPrice, UnitsInStock
+FROM products
+WHERE CategoryID = 8;
+
+-- Q11. What are the first and last names of all of the Northwind employees?
+SELECT FirstName, LastName
+FROM employees;
+
+-- Q12. What employees have "manager" in their titles?
+SELECT FirstName, LastName, Title
+FROM employees
+WHERE Title LIKE '%Manager%' ;
+
+-- Q13.List the distinct job titles in employees.
+
+
+-- Q14. What employees have a salary that is between $200 0 and $2500?
+
+
+-- Q15.List all the information about all of Northwind's suppliers.
+
+
+-- Q16.Examine the Products table. How do you know what supplier supplies each product? Write a query to list all the items that "Tokyo Traders"
+-- supplies to Northwind
