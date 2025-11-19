643f87f2e551e813c05f442bb659a4e4cccdb6b4
Judy Sitero <siterojudith@gmail.com>
2025-11-19 14:59:45 -0800

    Add files via upload

---
+++ nested-queries.sql
@@ -0,0 +1,48 @@
+-- EXERCISE -NESTED QUERIES (PG.76)
+
+-- Q1. What is the product name(s) of the most expensive products? HINT: Find the max price in a subquery
+-- and then use that value to find products whose price equals that value.
+SELECT ProductName
+FROM products
+WHERE UnitPrice = (SELECT MAX(UnitPrice) FROM products); 
+
+
+-- Q2. What is the order id, shipping name and shipping address of all orders shipped via "Federal Shipping"?
+--  HINT: Find the shipper id of "Federal Shipping" in a subquery and then use that value to find the orders that
+-- used that shipper.
+SELECT OrderID, ShipName, ShipAddress
+FROM orders
+WHERE ShipVia = (
+SELECT ShipperID 
+FROM Shippers 
+WHERE CompanyName = 'Federal Shipping'
+); 
+
+
+-- Q3. What are the order ids of the orders that ordered "Sasquatch Ale"? HINT: Find the product id of "Sasquatch Ale" 
+-- in a subquery and then use that value to find the matching orders from the `order details` table. Because the
+-- `order details` table has a space in its name, you will need to surround it with back ticks in the FROM clause.
+SELECT OrderID
+FROM  `order details`
+WHERE ProductID = (
+SELECT ProductID
+FROM Products
+WHERE ProductName = 'Sasquatch Ale'
+);
+
+
+-- Q4. What is the name of the employee that sold order 10266?
+SELECT FirstName, LastName
+FROM orders AS o
+JOIN employees AS e 
+ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderID = 10266;
+
+
+
+-- Q5. What is the name of the customer that bought order 10266?
+SELECT CompanyName
+FROM orders AS o
+JOIN customers AS c
+ON o.CustomerID = c.CustomerID
+WHERE o.OrderID = 10266;
